a0c1ddb462297d2817f7e53ca1c314f75b0259f2
rojojun <zoloman316@gmail.com>
2023-01-31 21:07:27 +0900

    feat : projet를 위한 패키지 설정

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <img src={logo} className="logo" alt="logo" />
+    <div id="wrapper">
+      <div className="logo-box">
+        <img src={logo} className="logo" alt="logo" />
+      </div>
     </div>
   );
 }
