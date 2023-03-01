8a28af59d8b5ee00fa75714b2857376b040f10b9
rojojun <zoloman316@gmail.com>
2023-03-01 22:31:58 +0900

    poke-kanto proj

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import logo from "./logo.png";
+import logo from "./asset/logo.png";
 import "./App.css";
+import innerlogo from "./asset/logo-2.GIF";
+import { Form } from "react-bootstrap";
 
 function App() {
   return (
     <div id="wrapper">
       <div className="logo-box">
         <img src={logo} className="logo" alt="logo" />
-        <p>HELLO</p>
+        <div className="inner-logo-box">
+          <img src={innerlogo} className="img" alt="img"></img>
+        </div>
       </div>
     </div>
   );
